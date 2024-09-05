8684b29e52c4fe0c18bb98d95b9df7a696c903a5
Norbert Koziana <koziana11norbert@gmail.com>
2024-09-05 11:59:08 +0200

    Initial commit

---
+++ backend/src/main/java/com/norbertkoziana/GitPen/GitPenApplication.java
@@ -0,0 +1,13 @@
+package com.norbertkoziana.GitPen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitPenApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/norbertkoziana/GitPen/GitPenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.norbertkoziana.GitPen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
