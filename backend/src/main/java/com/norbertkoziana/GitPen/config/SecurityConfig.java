5d9d4a57f63377022ca1862b81786730fd2e117c
Norbert Koziana <koziana11norbert@gmail.com>
2024-09-25 15:00:49 +0200

    revert last change

--- backend/src/main/java/com/norbertkoziana/GitPen/config/SecurityConfig.java
+++ backend/src/main/java/com/norbertkoziana/GitPen/config/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
         SimpleUrlAuthenticationFailureHandler handler = new SimpleUrlAuthenticationFailureHandler("/oauth2/error");
 
         CookieCsrfTokenRepository cookieCsrfTokenRepository = CookieCsrfTokenRepository.withHttpOnlyFalse();
-        cookieCsrfTokenRepository.setCookieCustomizer((x) -> x.sameSite(Cookie.SameSite.NONE.attributeValue()).secure(false));
+        cookieCsrfTokenRepository.setCookieCustomizer((x) -> x.sameSite(Cookie.SameSite.NONE.attributeValue()));
 
         http
                 .csrf((csrf) -> csrf
